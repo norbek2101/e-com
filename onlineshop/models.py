a62844806eee5fcba4be377c9628f0ddaf6a6814
norbek2101 <lightnight1223@gmail.com>
2021-12-29 10:51:38 +0500

    Category and Product models created then migrated

--- onlineshop/models.py
+++ onlineshop/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to="products/%Y/%M/%d", blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-name']
+        index_together = (('id', 'slug'),)
