78bb70dde97c7bf90f3b447abc8e4db850fa2ab1
norbek2101 <lightnight1223@gmail.com>
2022-01-15 12:39:43 +0500

    cart app and cart.py created

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'onlineshop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
